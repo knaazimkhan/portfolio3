9d95f0c30cc5a37ba4ac9447432fc4a5b3fdf264
Naazim Khan <knaazimkhan@gmail.com>
2025-01-04 15:08:03 +0400

    feat: add skeleton loading state for About section

--- src/components/lazy.tsx
+++ src/components/lazy.tsx
@@ -1,43 +1,42 @@
 import dynamic from 'next/dynamic';
-
-const Loading = () => <div>Loading...</div>;
+import { AboutSkeleton } from '@/components/ui/about-skeleton';
 
 export const AboutSection = dynamic(
   () => import('@/components/sections/AboutSection').then((mod) => mod.AboutSection),
-  { loading: Loading }
+  { loading: () => <AboutSkeleton /> }
 );
 
 export const BlogSection = dynamic(
   () => import('@/components/sections/BlogSection').then((mod) => mod.BlogSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const ProjectsSection = dynamic(
   () => import('@/components/sections/ProjectsSection').then((mod) => mod.ProjectsSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const SkillsSection = dynamic(
   () => import('@/components/sections/SkillsSection').then((mod) => mod.SkillsSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const ExperienceSection = dynamic(
   () => import('@/components/sections/ExperienceSection').then((mod) => mod.ExperienceSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const EducationSection = dynamic(
   () => import('@/components/sections/EducationSection').then((mod) => mod.EducationSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const TestimonialsSection = dynamic(
   () => import('@/components/sections/TestimonialsSection').then((mod) => mod.TestimonialsSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 );
 
 export const ContactSection = dynamic(
   () => import('@/components/sections/ContactSection').then((mod) => mod.ContactSection),
-  { loading: Loading }
+  { loading: () => <div>Loading...</div> }
 ); 
